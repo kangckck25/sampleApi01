899b954f969d9c3e6c27924e97a809c1136c78d2
kangckck25 <kangckck25@gmail.com>
2022-08-19 14:34:55 +0900

    Initial commit

---
+++ src/test/java/org/zerock/sampleapi01/SampleApi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.sampleapi01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApi01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
